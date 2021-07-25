5aadc942e55aa0e46eedc4fa1889b6cc4c8040c6
Josué Galeano <benyamin.galeano@gmail.com>
2021-07-25 12:36:19 -0600

    Remove tokyonight theme

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -5,17 +5,17 @@ utils.opt('o', 'termguicolors', true)
 cmd [[colorscheme monokai]]
 
 require'bufferline'.setup{}
-vim.g.tokyonight_style = "night"
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+-- vim.g.tokyonight_style = "night"
+-- vim.g.tokyonight_italic_functions = true
+-- vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 
 -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+-- vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
 
 require('lualine').setup {
   options = {
     -- ... your lualine config
-    theme = 'tokyonight',
+    -- theme = 'tokyonight',
     -- ... your lualine config
     extensions = { 'nerdtree' }
   }
